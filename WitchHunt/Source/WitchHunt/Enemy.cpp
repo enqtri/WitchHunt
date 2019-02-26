0b6788378c78860eb38ccb8bda220ae5e5e96e32
enqtri <47555800+enqtri@users.noreply.github.com>
2019-02-26 09:40:34 +0100

    Add files via upload

---
+++ WitchHunt/Source/WitchHunt/Enemy.cpp
@@ -0,0 +1,60 @@
+#include "Enemy.h"
+#include "MouseTurn.h"
+#include "MyPawn.h"
+
+#include "Components/BoxComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+
+// Sets default values
+AEnemy::AEnemy()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+    
+    ///A standard box collider with Overlap Events:
+    RootBox = CreateDefaultSubobject<UBoxComponent>(TEXT("MyEnemy"));
+    RootComponent = RootBox;
+    RootBox->SetGenerateOverlapEvents(true);
+    //Visual component added in blueprint
+}
+
+// Called when the game starts or when spawned
+void AEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+    MoveDirection = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation() - GetActorLocation();
+    MoveDirection.Normalize();
+    SetActorRotation(MoveDirection.Rotation());
+}
+
+// Called every frame
+void AEnemy::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+    
+    FVector NewLocation = GetActorLocation();
+    NewLocation += (MoveDirection * Speed * DeltaTime);
+    SetActorLocation(NewLocation);
+    
+    CurrentTurnDelay -= DeltaTime;
+    ///Turns the enemy after some time:
+    if (CurrentTurnDelay < 0.f)
+    {
+        MoveDirection = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation() - GetActorLocation();
+        MoveDirection.Normalize();
+        SetActorRotation(MoveDirection.Rotation());
+    
+        CurrentTurnDelay = FMath::FRandRange(TurnDelayMin, TurnDelayMax);
+    }
+}
+
+void AEnemy::ImHit()
+{
+    ///Must cast a pawn to the correct type to set enemies shot
+    ///Could have used GameMode or something else also to store this
+    AMyPawn* TempPawn = Cast<AMyPawn>(GetWorld()->GetFirstPlayerController()->GetPawn());
+    TempPawn->EnemiesShot++;
+    Destroy();
+}
---
+++ WitchHunt/Source/WitchHunt/Enemy.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Enemy.generated.h"
+
+class UShapeComponent;
+
+UCLASS()
+class MOUSETURN_API AEnemy : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemy();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+    
+    FVector MoveDirection = FVector(1.f, 0.f, 0.f);
+    
+    //When enemy is hit
+    void ImHit();
+    
+private:
+    
+    /**The speed of the enemy*/
+    UPROPERTY(EditAnywhere)
+    float Speed = 150.f;
+    
+    /**How long max till turn towards player*/
+    UPROPERTY(EditAnywhere)
+    float TurnDelayMax = 5.f;
+
+    /**How long min till turn towards player*/
+    UPROPERTY(EditAnywhere)
+    float TurnDelayMin = 1.f;
+    
+    float CurrentTurnDelay = 0.f;
+    
+    /**The collider component of the enemy*/
+    UPROPERTY(EditAnywhere)
+    UShapeComponent* RootBox = nullptr;
+};
